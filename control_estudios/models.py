bc35ba7cb0a12e0edd517dd1a1165f2fe2f95e4b
Santiago <santiagofryd@gmail.com>
2023-07-22 18:05:28 -0300

    Configuracion panel administrativo

--- control_estudios/models.py
+++ control_estudios/models.py
@@ -6,6 +6,9 @@ class Curso(models.Model):
 # los atributos de clase (son las columnas de la tabla)
     nombre = models.CharField(max_length=64)
     comision = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.nombre}"
 
 class Estudiante(models.Model):
     apellido = models.CharField(max_length=256)
@@ -15,6 +18,9 @@ class Estudiante(models.Model):
     dni = models.CharField(max_length=32)
     fecha_nacimiento = models.DateField(null=True)
 
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
+
 class Profesor(models.Model):
     apellido = models.CharField(max_length=256)
     nombre = models.CharField(max_length=256)
@@ -23,3 +29,15 @@ class Profesor(models.Model):
     fecha_nacimiento = models.DateField(null=True, blank=True)
     profesion = models.CharField(max_length=128)
     bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
+
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=256)
+    fecha_entrega = models.DateTimeField(auto_now_add=True)
+    esta_aprobado = models.BooleanField(default=False) 
+
+    def __str__(self):
+        return f"{self.nombre}"
